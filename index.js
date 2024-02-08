63fd1e7294f19dc83cb35e2f66455af1ed58417d
CarlosEHWendler <159396060+CarlosEHWendler@users.noreply.github.com>
2024-02-07 23:27:46 -0300

    Infância

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a sintaxe correta para comentários de uma única linha em JavaScript?",
-      respostas: [
-        "// Comentário",
-        "/* Comentário */",
-        "' Comentário",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a maneira correta de declarar uma variável em JavaScript?",
-      respostas: [
-        "var myVar;",
-        "let myVar;",
-        "both var and let",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Como você escreve 'Hello World' em um alerta em JavaScript?",
-      respostas: [
-        "msgBox('Hello World');",
-        "alertBox('Hello World');",
-        "alert('Hello World');",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a forma correta de escrever um loop em JavaScript?",
-      respostas: [
-        "for (i = 0; i <= 5; i++)",
-        "for (i <= 5; i++)",
-        "for (i = 0; i <= 5)",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual método é usado para imprimir algo no console em JavaScript?",
-      respostas: [
-        "console.log()",
-        "print()",
-        "log()",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a sintaxe correta para adicionar um comentário em várias linhas em JavaScript?",
-      respostas: [
-        "// Comentário",
-        "/* Comentário */",
-        "' Comentário",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a maneira correta de escrever uma função em JavaScript?",
-      respostas: [
-        "function: myFunction() {}",
-        "function myFunction() {}",
-        "function = myFunction() {}",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a forma correta de atribuir um valor a uma variável em JavaScript?",
-      respostas: [
-        "myVar == 1",
-        "myVar = 1",
-        "myVar === 1",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a função utilizada para converter uma string em um número em JavaScript?",
-      respostas: [
-        "parseInt()",
-        "stringToNumber()",
-        "toNumber()",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a maneira correta de comparar dois valores em JavaScript?",
-      respostas: [
-        "x === y",
-        "x = y",
-        "x == y",
-      ],
-      correta: 0
-    }
-  ];
+  {
+    pergunta: "Qual das seguintes atividades é mais comum entre as crianças nos dias atuais?",
+    respostas: [
+      "Brincar ao ar livre",
+      "Passar horas assistindo TV ou jogando videogame",
+      "Participar de atividades extracurriculares",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais fatores têm contribuído para mudanças na infância contemporânea?",
+    respostas: [
+      "Menos acesso à tecnologia",
+      "Falta de acesso à educação",
+      "Avanços tecnológicos e mudanças culturais",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é um desafio enfrentado pelas crianças nos dias de hoje?",
+    respostas: [
+      "Falta de oportunidades de aprendizado",
+      "Excesso de acesso à informação",
+      "Isolamento social devido ao uso excessivo de tecnologia",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é uma preocupação comum dos pais sobre a infância nos dias atuais?",
+    respostas: [
+      "Falta de acesso a brinquedos",
+      "Exposição precoce a conteúdo inadequado na internet",
+      "Limitação das opções de entretenimento",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Como as redes sociais têm influenciado a infância contemporânea?",
+    respostas: [
+      "Promovendo interações saudáveis entre as crianças",
+      "Contribuindo para uma compreensão mais equilibrada do mundo",
+      "Expondo as crianças a problemas como o cyberbullying",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é essencial para garantir um desenvolvimento saudável na infância atual?",
+    respostas: [
+      "Exposição mínima à tecnologia",
+      "Acesso ilimitado a dispositivos eletrônicos",
+      "Um equilíbrio entre tempo de tela e atividades ao ar livre",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Como os pais podem ajudar as crianças a lidar com o estresse relacionado ao mundo digital?",
+    respostas: [
+      "Ignorando o problema e esperando que ele desapareça",
+      "Comunicando-se abertamente e estabelecendo limites claros",
+      "Encorajando o uso constante de dispositivos eletrônicos",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é uma consequência potencial do uso excessivo de tecnologia na infância?",
+    respostas: [
+      "Melhoria da capacidade cognitiva",
+      "Desenvolvimento de habilidades sociais robustas",
+      "Problemas de saúde mental, como ansiedade e depressão",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é importante considerar ao introduzir dispositivos eletrônicos na vida de uma criança?",
+    respostas: [
+      "A quantidade de dinheiro que será gasta",
+      "O quanto isso facilitará a vida dos pais",
+      "Os possíveis impactos na saúde e no desenvolvimento da criança",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é uma estratégia eficaz para incentivar uma infância equilibrada nos dias atuais?",
+    respostas: [
+      "Privar completamente as crianças do acesso à tecnologia",
+      "Oferecer uma variedade de experiências, incluindo atividades ao ar livre e uso moderado de tecnologia",
+      "Permitir que as crianças passem a maior parte do tempo em frente a telas",
+    ],
+    correta: 1
+  }
+];
+
   
   const quiz = document.querySelector('#quiz')
   
